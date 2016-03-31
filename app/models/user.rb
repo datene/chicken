0875c5c652d3841952517980b7fb43aba48d896c
Michele de Bruyn <debruyn.mm@gmail.com>
2016-03-31 16:55:32 +0200

    Fix seed stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,17 +12,24 @@ class User < ActiveRecord::Base
          :omniauthable, omniauth_providers: [:facebook]
 
   def self.find_for_facebook_oauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]  # Fake password for validation
-      user.first_name = auth.info.first_name
-      user.last_name = auth.info.last_name
-      user.picture = auth.info.image
-      user.token = auth.credentials.token
-      user.token_expiry = Time.at(auth.credentials.expires_at)
+    user = User.find_by_email(auth.info.email)
+
+    unless user
+      user = User.where(provider: auth.provider, uid: auth.uid).first_or_initialize 
     end
+
+    user.provider = auth.provider
+    user.uid = auth.uid
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]  # Fake password for validation
+    user.first_name = auth.info.first_name
+    user.last_name = auth.info.last_name
+    user.picture = auth.info.image
+    user.token = auth.credentials.token
+    user.token_expiry = Time.at(auth.credentials.expires_at)
+
+    user.save
+    user
   end
 
   def all_challenges
